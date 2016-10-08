76d0e9ca9241dfdd7f73c98ebde0f92aec6ff42a
Titania Putri <titaniaputri28@gmail.com>
2016-10-08 20:01:50 +0700

    startActivity Part 2

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/HasilActivity.java
@@ -1,9 +1,11 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class HasilActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class HasilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
+        String nama = getIntent().getStringExtra(MainActivity.NAMA);
+        int umur = getIntent().getIntExtra(MainActivity.UMUR,0);
+        int yearNow = Calendar.getInstance().get(Calendar.YEAR);
+        int tahunLahir = yearNow - umur;
+        TextView tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil.setText(nama + " lahir pada tahun" + tahunLahir);
+        setTitle("Hasil");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         findViewById(R.id.buttonBack).setOnClickListener(new View.OnClickListener()
         {
